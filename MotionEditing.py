259db3b8a495d0cb404cdfdd7ba282593831e7d7
Gao Yuan <gao131@mcmaster.ca>
2024-03-03 00:45:38 -0500

    Update MotionEditing.py
    wrote the concatenate part, changed code output

--- MotionEditing.py
+++ MotionEditing.py
@@ -33,7 +33,26 @@ no need to change the skeleton, which is anim.orients and anim.offsets
 
 #TODO: smoothly connects the two motions
 def concatenate(anim1, anim2):
-    pass
+    # the frame is 30
+    i = 0
+    E1 = anim1.rotations[-30, 0]
+    S2 = anim2.rotations[0, 0]
+    w, x, y, z = S2.qs[0]
+    conj = Q.Quaternions(np.array([w, -x, -y, -z]))
+    modu_squared = w ** 2 + x ** 2 + y ** 2 + z ** 2
+    S2inv = conj / modu_squared
+    anim2 = rotate_root(anim2, E1 * S2inv)
+    anim2 = relocate(anim2, anim1.positions[-30, 0])
+    blend_p = np.zeros((30, anim1.positions.shape[1], 3))
+    blend_r = np.zeros((30, anim1.rotations.shape[1], 4))
+    while i < 30:
+        t = i / 29
+        blend_p[i] = anim1.positions[i-30] * (1 - t) + anim2.positions[i] * t
+        blend_r[i] = Q.Quaternions.slerp(anim1.rotations[i-30], anim2.rotations[i], t)
+        i+=1
+    r3 = Q.Quaternions(np.vstack((anim1.rotations.qs[:-30], blend_r, anim2.rotations.qs[31:])))
+    p3 = np.vstack((anim1.positions[:-30], blend_p, anim2.positions[31:]))
+    return A.Animation(r3, p3, anim1.orients, anim1.offsets, anim1.parents)
 
 #TODO: splice right arm waving from anim2 into anim1 walking
 def splice(anim1, anim2):
@@ -49,10 +68,10 @@ anim_walk, joint_names_walk, frametime_walk = BVH.load(filename_walk)
 filename_wave = filepath + '111_37.bvh'
 anim_wave, joint_names_wave, frametime_wave = BVH.load(filename_wave)
 
-'''simple editing'''
-anim_concat_naive = concatenate_naive(anim_walk, anim_wave)
-filename_concat_naive = filepath + 'concat_naive.bvh'
-BVH.save(filename_concat_naive, anim_concat_naive, joint_names_walk, frametime_walk)
+'''concatenate two animation slip'''
+anim_concatenated = concatenate(anim_walk, anim_wave)
+filename_concatenated = filepath + 'result_concatenated.bvh'
+BVH.save(filename_concatenated, anim_concatenated, joint_names_walk, frametime_walk)
 
 anim_rotated = rotate_root(anim_walk, Q.Quaternions.from_euler(np.array([0, 90, 0])))
 filename_rotated = filepath + 'rotated.bvh'
