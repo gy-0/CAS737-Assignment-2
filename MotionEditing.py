e67362cd89bf887c2ba3b163ad6d6c3458eb7a12
Gao Yuan <gao131@mcmaster.ca>
2024-03-02 23:39:22 -0500

    Update MotionEditing.py
    Apply delta R

--- MotionEditing.py
+++ MotionEditing.py
@@ -5,14 +5,17 @@ import numpy as np
 
 '''relocate anim so that it starts from start_pos'''
 def relocate(anim, start_pos):
+    # pos: 3-dimension point
     delta_pos = start_pos - anim.positions[0, 0]
     anim.positions[:,0] += delta_pos
     return anim
 
 '''rotate anim, so that it starts in different facing direction, delta_q is quaternion'''
+# reorient: apply delta R to root rotation of all frames
 def rotate_root(anim, delta_q):
     anim.rotations[:, 0] = delta_q * anim.rotations[:, 0]
     transform = np.repeat(delta_q.transforms(), anim.shape[0], axis=0)
+    # Apply delta R to adjust root translation
     for f in range(0, anim.shape[0]):
         anim.positions[f,0] = np.matmul(transform[f], anim.positions[f,0])
     return anim
@@ -22,7 +25,6 @@ def concatenate_naive(anim1, anim2):
     anim1.rotations = Q.Quaternions(np.vstack((anim1.rotations.qs, anim2.rotations.qs)))
     anim1.positions = np.vstack((anim1.positions, anim2.positions))
     return anim1
-
 
 '''NOTE 
 for motion editing, only change anim.rotations and anim.positions
